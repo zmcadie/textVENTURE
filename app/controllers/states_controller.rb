aba6a5db0cc17080376a7582aff15e455c9d9e10
Zola <zolamcadie@gmail.com>
2017-08-10 22:33:12 +0000

    Create actions for update, show, index in states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,26 @@
 class StatesController < ApplicationController
-
-  def show
-    @state = State.find(1)
+  def index
+    redirect_to action: 'show', id: 1
   end
 
+  def show
+    @state = State.find(params[:id])
+    session[:state_id] = params[:id]
+    @session = session[:state_id]
+  end
+
+  def update
+    @action = form_params
+    @state_id = Action.where({ state_id: @action[:state_id], trigger: @action[:trigger] }).first.result_id
+    redirect_to action: 'show', id: @state_id
+  end
+
+  private
+
+  def form_params
+    params.require(:cats).permit(
+      :trigger,
+      :state_id
+    )
+  end
 end
