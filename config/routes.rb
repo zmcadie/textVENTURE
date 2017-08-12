d31c14bad30e1a542e5d533a18495ded61077fd2
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-12 22:37:36 +0000

    Change name of params to :game_id and :state_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'games#index'
-  get '/games/:id' => 'games#show'
-  get 'games/:id/states/:id' => 'states#show'
+  get '/games/:game_id' => 'games#show'
+  get 'games/:game_id/states/:state_id' => 'states#show'
   post '/' => 'states#update'
 end
