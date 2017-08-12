63ed45b31e53bfee171f43425352f6fb48837d17
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-12 20:16:03 +0000

    Fix typo in Game.destroy_all to run seeds successfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-Game.destroy.all
+Game.destroy_all
 @game1 = Game.create! id: 1, name: "Harry Potter Maze"
 
 State.destroy_all
