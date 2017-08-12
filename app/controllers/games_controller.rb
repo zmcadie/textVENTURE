5bb44c1868f47fcc20249e186e2299b747308872
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-12 20:56:55 +0000

    Move index and show from states controller to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+  @@state_log = []
+
+  def index
+    if session[:state_id]
+      redirect_to action: 'show', id: session[:state_id]
+    else
+      @@state_log.push(State.find(1).description)
+      redirect_to action: 'show', id: 1
+    end
+  end
+
+  def show
+    state = State.find(params[:id])
+    session[:state_id] = params[:id]
+    @log = @@state_log
+    @session = session[:state_id]
+  end
 end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,22 +1,6 @@
 class StatesController < ApplicationController
   @@state_log = []
 
-  def index
-    if session[:state_id]
-      redirect_to action: 'show', id: session[:state_id]
-    else
-      @@state_log.push(State.find(1).description)
-      redirect_to action: 'show', id: 1
-    end
-  end
-
-  def show
-    state = State.find(params[:id])
-    session[:state_id] = params[:id]
-    @log = @@state_log
-    @session = session[:state_id]
-  end
-
   def update
     action = form_params
     @@state_log.push(">> #{action[:trigger]}")
