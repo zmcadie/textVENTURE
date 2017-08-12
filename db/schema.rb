f241c6ce630e39c98e0f51456b035b59e9d119db
Madeline Collier <madel@telus.net>
2017-08-12 21:20:22 +0000

    Create and run migration to add inital state column to Games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812195345) do
+ActiveRecord::Schema.define(version: 20170812211755) do
 
   create_table "actions", force: :cascade do |t|
     t.integer "state_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170812195345) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "initial_state_id"
   end
 
   create_table "states", force: :cascade do |t|
