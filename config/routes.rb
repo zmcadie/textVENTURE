3a9d1072b9ba20ce521eac09381ee9b97d62f64d
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-12 21:38:23 +0000

    Add link to games#show to redirect to games/1/states/1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'games#index'
-  get '/:id' => 'states#show'
-  post '/' => 'states#update'
   get '/games/:id' => 'games#show'
+  get 'games/:id/states/:id' => 'states#show'
+  post '/' => 'states#update'
 end
