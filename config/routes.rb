ec07dc38f790b21ac6a2c59562d605ee7182bfd6
Madeline Collier <madel@telus.net>
2017-08-13 20:15:33 +0000

    Create post route /games for selecting which game to play when posting from games index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/games/:game_id' => 'games#show'
   get 'games/:game_id/states/:state_id' => 'states#show'
   post '/' => 'states#update'
+  post '/games' => 'games#select'
 end
