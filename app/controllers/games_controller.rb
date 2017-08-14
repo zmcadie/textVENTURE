a0a54534c1e2f43763139b9a77ee7fdcf63bac4a
Madeline Collier <madel@telus.net>
2017-08-14 20:28:54 +0000

    Pass @games variable to index view from games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,7 @@ class GamesController < ApplicationController
     if session[:state_id]
       redirect_to "/games/#{session[:game_id]}/states/#{session[:state_id]}"
     else
-      display_games_index
-      @log = @@state_log
+      @games = display_games_index
     end
   end
 
@@ -39,11 +38,11 @@ class GamesController < ApplicationController
   end
 
   def display_games_index
-    update_state_log('Welcome to textVENTURE! Please choose a game from the selection below:')
+    index = ['Welcome to textVENTURE! Please choose a game from the selection below:']
     Game.all.each do |game|
-      update_state_log(game.name)
+      index.push(game.name)
     end
-    update_state_log('Simply type the name of the game you wish to play, and hit enter')
+    index.push('Simply type the name of the game you wish to play, and hit enter')
   end
 
   def game_selection_form
