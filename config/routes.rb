93e4b26154bc94ae34cd5938352f2e8af2473547
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-14 21:35:33 +0000

    Add routes for 3-page new game form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   get '/' => 'games#index'
-  get '/games/new' => 'games#new'
-  post '/games/new' => 'games#new'
   get '/games/:game_id' => 'games#show'
   get 'games/:game_id/states/:state_id' => 'states#show'
   post '/' => 'states#update'
   post '/games' => 'games#select'
+
+  # form for adding game to db
+  get '/games/new' => 'games#new'
+  post '/games/new' => 'games#new'
+  # form for adding states to game
+  get '/games/:new_id/states' => 'games#states'
+  post '/games/:new_id/states' => 'games#states'
+  # view for individual states already created (sidebar links)
+  get '/games/:new_id/states/:state_id' => 'games#states'
+  # form for adding actions to states via 'connections'
+  get '/games/:new_id/states/:state_id/connections' => 'games#connections'
+  post '/games/:new_id/states/:state_id/connections/:connection_id' => 'games#connections'
 end
