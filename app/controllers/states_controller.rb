a4a47fc6c8166442070b111071419e0dbdafb713
Madeline Collier <madel@telus.net>
2017-08-15 21:57:23 +0000

    Move approx_trigger? method from states into games controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -8,17 +8,6 @@ class StatesController < GamesController
 
   private
 
-  def aprox_trigger?(user_input)
-    next_state_id = nil
-    Action.where({ state_id: session['state_id'] }).find_each do |action|
-      trigger_words = action.trigger.split
-      if trigger_words.any? { |word| user_input.include?(word) }
-        next_state_id = action.result_id
-      end
-    end
-    next_state_id
-  end
-
   def form_params
     params.require(:user_input).permit(
       :trigger,
