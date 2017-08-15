104103feba25630414157fe2439dd765894c1fed
Madeline Collier <madel@telus.net>
2017-08-15 21:41:57 +0000

    Move actions_helper method out of states controller and into games controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,12 +3,10 @@ class StatesController < GamesController
   def update
     action = form_params
     update_state_log(action[:trigger])
+    handle_user_input(action[:trigger])
     clean_trigger = clean_user_input(form_params['trigger'])
 
-    if clean_trigger == 'help'
-      actions_helper
-      state_id = session[:state_id]
-    elsif aprox_trigger?(clean_trigger)
+    if aprox_trigger?(clean_trigger)
       new_state_id = aprox_trigger?(clean_trigger)
       session[:state_id] = new_state_id
       description = State.find(new_state_id).description
@@ -17,8 +15,9 @@ class StatesController < GamesController
       state_id = session[:state_id]
       update_state_log('Sorry I don\'t know what that means')
     end
-
-    redirect_to "/games/#{session[:game_id]}/states/#{session[:state_id]}"
+    if not performed?
+      redirect_to "/games/#{session[:game_id]}/states/#{session[:state_id]}"
+    end
   end
 
   private
@@ -32,16 +31,6 @@ class StatesController < GamesController
       end
     end
     next_state_id
-  end
-
-  def actions_helper
-    available_actions = ""
-    Action.where({ state_id: session['state_id'] }).find_each do |trigger|
-      available_actions += trigger.trigger + " "
-    end
-    action = "Maybe try: #{available_actions}"
-    update_state_log(action)
-    action
   end
 
   def form_params
