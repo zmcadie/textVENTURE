38cc4fcacbc1c39d736568a79c5cafbc773368e7
Zola <zolamcadie@gmail.com>
2017-08-15 22:12:44 +0000

    Display log items with different styling depending on item type

--- app/javascript/Console.jsx
+++ app/javascript/Console.jsx
@@ -4,7 +4,17 @@ class Console extends Component {
   render() {
 
     const logs = this.props.log.map((log, i) => {
-      return <div key={i}>{log}</div>
+      switch (log.type) {
+        case 'game':
+          return <div className="game-log" key={i}>{log.value}</div>
+          break
+        case 'user':
+          return <div className="user-log" key={i}>>> {log.value}</div>
+          break
+        case 'system':
+          return <div className="system-log" key={i}>{log.value}</div>
+          break
+      }
     })
 
     return(
