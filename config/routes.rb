b5a657c259a1a1c1a8aca143ffe7db0df7083820
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-15 02:34:34 +0000

    Modify show route to display each state's info

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/games/new/:new_id/states' => 'games#states'
   post '/games/new/:new_id/states' => 'games#states_create'
   # view for individual states already created (sidebar links)
-  get '/games/new/:new_id/states/:state_id' => 'games#states'
+  get '/games/new/:new_id/states/:state_id' => 'games#states_show'
   # form for adding actions to states via 'connections'
   get '/games/new/:new_id/states/:state_id/connections' => 'games#connections'
   post '/games/new/:new_id/states/:state_id/connections/:connection_id' => 'games#connections'
