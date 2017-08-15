1355e290de990ebaa8c3db17aa05b40ee24b78ae
Zola <zolamcadie@gmail.com>
2017-08-15 00:24:33 +0000

    Remove presence validation from initial_state_id in games model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   has_many :states
 
-  validates :name, :initial_state_id, presence: true
+  validates :name, presence: true
   validates :name, uniqueness: true
 end
