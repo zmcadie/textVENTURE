feaddb6a0ba6d0a958a9c792575745c033267ac1
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-17 22:08:05 +0000

    Specify action in create game form, matching post route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # form for adding game to db
   get '/games/new' => 'games#new'
   post '/games/new' => 'games#create'
+  post 'games/new' => 'games#save_game'
   # form for adding actions to states via 'connections'
   get '/games/new/:new_id/states/connections' => 'games#connections'
   get '/games/new/:new_id/states/:state_id/connections' => 'games#connections_show'
