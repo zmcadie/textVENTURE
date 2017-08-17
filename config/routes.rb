0c30234724265f5129f0ee0d74419f03e4b7459d
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-17 22:20:38 +0000

    Modify post route for save_game action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # form for adding game to db
   get '/games/new' => 'games#new'
   post '/games/new' => 'games#create'
-  post 'games/new' => 'games#save_game'
   # form for adding actions to states via 'connections'
   get '/games/new/:new_id/states/connections' => 'games#connections'
   get '/games/new/:new_id/states/:state_id/connections' => 'games#connections_show'
@@ -13,6 +12,8 @@ Rails.application.routes.draw do
   post '/games/new/:new_id/states' => 'games#states_create'
   # view for individual states already created (sidebar links)
   get '/games/new/:new_id/states/:state_id' => 'games#states_show'
+  # button to toggle publish to true in db
+  post 'games/new/:new_id' => 'games#save_game'
 
   get '/games/:game_id' => 'games#show'
   post '/' => 'states#update'
