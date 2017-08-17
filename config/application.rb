9cc802bad61114bd8163c36d6f6c94565f7d48e0
Zola <zolamcadie@gmail.com>
2017-08-17 23:03:36 +0000

    switch to pg from sqlite3 for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TextVENTURE
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.web_console.whitelisted_ips = '10.0.2.0/16'
+    # config.web_console.whitelisted_ips = '10.0.2.0/16'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
