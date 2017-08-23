28a951406bbed75646e2437e2105aa411e56db56
Zola <zolamcadie@gmail.com>
2017-08-23 20:42:44 +0000

    Add save state model validations

--- app/models/save_state.rb
+++ app/models/save_state.rb
@@ -1,2 +1,11 @@
 class SaveState < ApplicationRecord
+  belongs_to :game
+  belongs_to :state
+
+  validates :user_email, presence: true
+  validates :game_id, presence: true
+  validates :user_id, presence: true
+
+  def save_game(email)
+  end
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,7 @@
 class State < ApplicationRecord
   belongs_to :game
   has_many :actions, dependent: :destroy
+  has_many :save_states, dependent: :destroy
 
   validates :name, :description, presence: true
 end
